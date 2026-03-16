1624dc4847819c1a7ea6190c0189c1970d37d5e1
Javier <javyervg@gmail.com>
2026-03-16 19:00:01 +0100

    chore: Initial Project Structure

---
+++ src/test/java/com/javiervidgar/chess_backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiervidgar.chess_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
